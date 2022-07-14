575bd5435fdf46afc7d29cbd5ff3a927572a5a6c
jaehun <510jaehun@bigcare.co.kr>
2022-07-14 10:27:15 +0900

    empty image ㅊㅓ리

--- app/src/main/java/com/oldee/expert/base/BaseViewModel.kt
+++ app/src/main/java/com/oldee/expert/base/BaseViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.oldee.expert.R
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel(var repository: BaseRepository) : ViewModel() {
@@ -12,9 +13,12 @@ abstract class BaseViewModel(var repository: BaseRepository) : ViewModel() {
 
     fun setImage(imageView: ImageView, url: String) {
         viewModelScope.launch {
-            val bitmap = repository.getImageFromServer(url)
+            val bitmap = repository.getImageFromServer("")
             if (bitmap != null) {
-                Glide.with(imageView.context).load(bitmap).into(imageView)
+                Glide.with(imageView.context).load(bitmap).placeholder(R.drawable.icon_empty_image)
+                    .error(R.drawable.icon_empty_image).into(imageView)
+            } else {
+                Glide.with(imageView.context).load(R.drawable.icon_empty_image).into(imageView)
             }
         }
     }
@@ -24,6 +28,10 @@ abstract class BaseViewModel(var repository: BaseRepository) : ViewModel() {
             val bitmap = repository.getImageFromServer(url)
             if (bitmap != null) {
                 Glide.with(imageView.context).load(bitmap).apply(RequestOptions().circleCrop())
+                    .placeholder(R.drawable.icon_empty_image).error(R.drawable.icon_empty_image)
+                    .into(imageView)
+            } else {
+                Glide.with(imageView.context).load(R.drawable.icon_empty_image).apply(RequestOptions().circleCrop())
                     .into(imageView)
             }
         }
